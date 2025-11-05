fe2d23f925249712c5039eae1363f990e919575c
Kuhnen123 <>
2025-11-05 12:07:37 -0300

    eta

--- login.js
+++ login.js
@@ -3,14 +3,17 @@
     const toggleIcon = document.getElementById('toggleIcon');
 
     toggleButton.addEventListener('click', function() {
-      // Alterna o tipo do input
+      
       const type = passwordInput.type === 'password' ? 'text' : 'password';
       passwordInput.type = type;
-      
-      // Alterna o ícone
+    
       if (type === 'password') {
         toggleIcon.textContent = 'visibility';
       } else {
         toggleIcon.textContent = 'visibility_off';
       }
     });
+
+    const MudarTela = document.getElementById('btn-primary');
+
+
